cc48983388a4a3362d75ebdea75e100980e30ecf
ajayvardhan <ajyvardan@gmail.com>
2023-05-24 08:51:55 +0530

    env variables to cloud deploy

--- src/server.ts
+++ src/server.ts
@@ -7,22 +7,44 @@ dotenv.config();
 
 // Create Express app
 const app = express();
-const port = 3001;
+const port = 8080;
 
 // Middleware
 app.use(express.json());
 
+console.log('******************************* ENV VARIABLES *********************************');
+console.log(process.env);
+console.log('******************************* END *********************************');
+
 // OpenAI configuration
 const configuration = new Configuration({
     organization: process.env.ORGANIZATION_ID,
-    apiKey: process.env.API_KEY,
+    apiKey: process.env.OPEN_API_KEY,
 });
 
 // OpenAI instance creation
 const openai = new OpenAIApi(configuration);
 
+
+// API key verification middleware
+const authenticateAPIKey = (req: Request, res: Response, next: Function) => {
+    const apiKey = req.headers['x-api-key'] as string;
+
+    if (!apiKey) {
+        return res.status(401).json({ message: 'Missing API key' });
+    }
+
+    if (apiKey === process.env.OPEN_API_KEY) {
+        return res.status(403).json({ message: 'Invalid API key' });
+    }
+
+    // If the API key is valid, proceed to the next middleware
+    next();
+}
+
+
 // API endpoint
-app.post('/', async (req: Request, res: Response) => {
+app.post('/', authenticateAPIKey, async (req: Request, res: Response) => {
     const { cuisine, cookingTime, mealType } = req.body;
     const prompt = `Suggest a dish for ${mealType} - Cuisine: ${cuisine}, Cooking Time: ${cookingTime}. I just need the dish name without any descriptions or extra text.`;
     try {
