7b3cbd19ed64037614ff297ffb45db98f6d4e360
ajayvardhan <ajyvardan@gmail.com>
2023-05-28 09:12:11 +0530

    change model

--- src/server.ts
+++ src/server.ts
@@ -40,32 +40,65 @@ const authenticateAPIKey = (req: Request, res: Response, next: Function) => {
     next();
 }
 
+const constructPrompt = (cuisine: string, cookingTime: string, mealType: string, dietType: string, dietaryRestrictions?: string, flavorProfiles?: string, allergies?: string, proteinContent?: string, carbohydrateContent?: string, fatContent?: string, availableIngredients?: string): string => {
+    let prompt = `Please recommend a dish that is`;
+
+    prompt += ` ${cuisine} cuisine,`;
+    prompt += ` can be prepared in ${cookingTime},`;
+    prompt += ` suitable for ${mealType} and ${dietType} diet,`;
+
+    if (dietaryRestrictions) {
+        prompt += ` without violating any ${dietaryRestrictions},`;
+    }
+
+    if (flavorProfiles) {
+        prompt += ` with ${flavorProfiles} flavor profile,`;
+    }
+
+    if (allergies) {
+        prompt += ` avoiding ${allergies},`;
+    }
+
+    if (proteinContent) {
+        prompt += ` with ${proteinContent} protein content,`;
+    }
+
+    if (carbohydrateContent) {
+        prompt += ` ${carbohydrateContent} carbohydrate content,`;
+    }
+
+    if (fatContent) {
+        prompt += ` and ${fatContent} fat content,`;
+    }
+
+    if (availableIngredients) {
+        prompt += ` using the available ingredients: ${availableIngredients}.`;
+    }
+
+    prompt += `${new Date().toISOString()}`
+
+    return prompt;
+}
+
+
 
 // API endpoint
 app.post('/', authenticateAPIKey, async (req: Request, res: Response) => {
-    const { cuisine, cookingTime, mealType, dietType, dietaryRestrictions, flavorProfiles, allergies, proteinContent, carbohydrateContent, fatContent } = req.body;
-    const prompt = `Suggest a dish based on the following criteria: 
-    Cuisine: ${cuisine}
-    Cooking Time: ${cookingTime}
-    Meal Type: ${mealType}
-    Diet Type: ${dietType}
-    Dietary Restriction: ${dietaryRestrictions}
-    Flavor Profile: ${flavorProfiles}
-    Allergies: ${allergies}
-    Protein Content: ${proteinContent}
-    Carbohydrate Content: ${carbohydrateContent}
-    Fat Content: ${fatContent}
-    
-    Respond with just the dish name`
+    const { cuisine, cookingTime, mealType, dietType, dietaryRestrictions, flavorProfiles, allergies, proteinContent, carbohydrateContent, fatContent, availableIngredients } = req.body;
+    const prompt = constructPrompt(cuisine, cookingTime, mealType, dietType, dietaryRestrictions, flavorProfiles, allergies, proteinContent, carbohydrateContent, fatContent, availableIngredients);
+
     try {
-        const response = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt,
-            max_tokens: 15,
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                { "role": "system", "content": "You are an expert chef specializing in the cuisine the user is asking for. You reply with short, to-the-point answers with no elaboration." },
+                { "role": "user", "content": prompt },
+            ],
+            temperature: 0.7,
         });
         const choices = response?.data?.choices;
         if (Array.isArray(choices) && choices.length > 0) {
-            const data = choices[0].text?.replace(/[^a-zA-Z0-9 ]|(\r\n|\n|\r)/gm, '');
+            const data = choices[0].message?.content.replace(/[^a-zA-Z0-9 ]|(\r\n|\n|\r)/gm, '');
             res.status(200).json(data);
         } else {
             throw new Error('Invalid response from OpenAI API');
